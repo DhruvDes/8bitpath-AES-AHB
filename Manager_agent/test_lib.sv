94f945f68e89c9d76825949e9cd7110dc3cb41e0
Dhruv Deshmukh. <dhruvdeshmukh1990@gmail.com>
2025-11-06 22:58:04 -0500

    git tutorial

--- Manager_agent/test_lib.sv
+++ Manager_agent/test_lib.sv
@@ -6,29 +6,18 @@ class ahb_base_test extends uvm_test;
     endfunction
     
     ahb_env env;
-	ahb_wr_rd_seq rw_seq;
+
   
     function void build_phase (uvm_phase phase);
         super.build_phase(phase);
 
         env = ahb_env :: type_id :: create("env", this);
-      	rw_seq = ahb_wr_rd_seq :: type_id :: create("rw_seq", this);
+      	
       
     endfunction 
   
 	
-  task run_phase(uvm_phase phase);
-    
-    phase.raise_objection(this);
-    
-    
-    rw_seq.start(env.Magnt.sqr);
-    
-    
-    phase.drop_objection(this);
-    
-  endtask
-  	
+
   
     function  void end_of_elaboration_phase(uvm_phase phase);
 
@@ -36,4 +25,35 @@ class ahb_base_test extends uvm_test;
         
     endfunction
 
+endclass
+
+
+
+class r_w_1_test extends ahb_base_test;
+  
+  `uvm_component_utils(r_w_1_test);
+    
+  function new (string name = "r_w_1_test", uvm_component parent);
+        super.new(name,parent);
+    endfunction
+    
+  ahb_wr_rd_seq rw_seq;
+  
+  function void build_phase (uvm_phase phase);
+        super.build_phase(phase);
+      	rw_seq = ahb_wr_rd_seq :: type_id :: create("rw_seq", this);
+      
+    endfunction 
+  
+  
+  
+ 
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    rw_seq.start(env.Magnt.sqr);
+    phase.drop_objection(this);  
+  endtask
+  	
+  
+  
 endclass
